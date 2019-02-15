f325cddb38b083a118e5bb2b748f8447b711dafd
Naiji Zhang <naijizhang@gmail.com>
2019-02-15 14:56:19 -0800

    Add Markup to Header and Footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -11,7 +11,20 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div>
+						<h3>Contact Info</h3>
+						<p>info@inhabitent.com</p>
+						<p>778-456-7891</p>
+						<p></p>
+					</div>
+					<div>
+						<h3>Business Hour</h3>
+						<p>Monday-Friday: 9am to 5pm</p>
+						<p>Saturday: 10am to 2pm</p>
+						<p>Sunday: Closed</p>
+					</div>
+					<?php $logo_text_url= get_template_directory_uri().'/assets/images/logos/inhabitent-logo-text-dark.svg';?>
+					<img src='<?php echo $logo_text_url?>'>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -22,16 +22,21 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html('Skip to content'); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-			<i class="fas fa-campground"></i>
+			
+			<?php $logo_url= get_template_directory_uri().'/assets/images/logos/inhabitent-logo-tent-white.svg';
+				$logo_large_url= get_template_directory_uri().'/assets/images/logos/inhabitent-logo-full.svg'?>
+				
 				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-					<p class="site-description"><?php bloginfo('description'); ?></p>
+				<img src='<?php echo $logo_url?>'>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html('Primary Menu'); ?></button>
 					<?php wp_nav_menu(array('theme_location' => 'primary', 'menu_id' => 'primary-menu')); ?>
 				</nav><!-- #site-navigation -->
+
+			
+				<img src='<?php echo $logo_large_url?>'>
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
