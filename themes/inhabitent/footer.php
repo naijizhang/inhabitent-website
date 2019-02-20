6fea1df2a0894311ca6022d65f2bdd2f700282f2
Naiji Zhang <naijizhang@gmail.com>
2019-02-20 15:16:58 -0800

    Add CSS for footer

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -7,11 +7,11 @@
 
 ?>
 
-			</div><!-- #content -->
+			
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<div>
+				
+					<div class="footer-contact-info">
 						<h3>Contact Info</h3>
 						<p><i class="fas fa-envelope-square"></i>info@inhabitent.com</p>
 						<p><i class="fas fa-phone"></i>778-456-7891</p>
@@ -19,17 +19,19 @@
 						<i class="fab fa-twitter-square"></i>
 						<i class="fab fa-google-plus-square"></i></p>
 					</div>
-					<div>
+					<div class="footer-business-hour">
 						<h3>Business Hour</h3>
 						<p>Monday-Friday: 9am to 5pm</p>
 						<p>Saturday: 10am to 2pm</p>
 						<p>Sunday: Closed</p>
 					</div>
+					<div class="footer-logo">
 					<?php $logo_text_url= get_template_directory_uri().'/assets/images/logos/inhabitent-logo-text-dark.svg';?>
 					<img src='<?php echo $logo_text_url?>'>
-				</div><!-- .site-info -->
+					</div>
+				
 			</footer><!-- #colophon -->
-		</div><!-- #page -->
+		
 
 		<?php wp_footer(); ?>
 
