2c300e0d1afc6e1eb8dfdf6ca3ec5c85a2ee47d0
Naiji Zhang <naijizhang@gmail.com>
2019-02-24 15:02:18 -0800

    Update front page adventure grid styling

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -7,69 +7,68 @@
  */
 
 get_header(); ?>
-<?php $logo_large_url = get_template_directory_uri() . '/assets/images/logos/inhabitent-logo-full.svg';?>
+<?php $logo_large_url = get_template_directory_uri() . '/assets/images/logos/inhabitent-logo-full.svg'; ?>
 <div class="logo-container">
-<img class="frontsite-header-logo" src='<?php echo $logo_large_url ?>'>
+    <img class="frontsite-header-logo" src='<?php echo $logo_large_url ?>'>
 </div>
-	<?php if (have_posts()) : while (have_posts()) : the_post();
-	the_content();
-	endwhile;
-	else : ?>
-	<p>Sorry, no posts matched your criteria.</p>
-	<?php endif; ?>
-	<div id="primary" class="content-area">
-		<main id="main" class="front-site-main" role="main">
+<?php if (have_posts()): while (have_posts()): the_post();
+        the_content();
+    endwhile;
+else: ?>
+<p>Sorry, no posts matched your criteria.</p>
+<?php endif; ?>
+<div id="primary" class="content-area">
+    <main id="main" class="front-site-main" role="main">
 
-		<!-- show product types -->
-		<?php $product_types = get_terms('product_type', array(
-		'hide_empty' => false,
-		'orderby' => 'name',
-		'order' => 'ASC'
-	)); ?>
-	<h2 class="heading-show-stuff">SHOP STUFF</h2>
-		<div class="product-types-container">
-			<?php foreach ($product_types as $product_type) : ?>
-			<div class="product-type-item">
-				<img class="product-type-logo" src=<?php echo inhabitent_get_product_type_logo($product_type->name . '.svg') ?> >
-				<p>
-					<?php echo $product_type->description ?>
-				</p>
-				<input type="button" value="<?php echo $product_type->name ?> Stuff" class="homebutton" id="go-to-post" 
-onClick="document.location.href='<?php echo esc_url( get_term_link($product_type))?>'" />
-			
-			</div>
-			<?php endforeach ?>
-		</div>
+        <!-- show product types -->
+        <?php $product_types = get_terms('product_type', array(
+            'hide_empty' => false,
+            'orderby' => 'name',
+            'order' => 'ASC'
+        )); ?>
+        <h2 class="heading-show-stuff">SHOP STUFF</h2>
+        <div class="product-types-container">
+            <?php foreach ($product_types as $product_type): ?>
+            <div class="product-type-item">
+                <img class="product-type-logo" src=<?php echo inhabitent_get_product_type_logo($product_type->name . '.svg') ?> >
+                <p>
+                    <?php echo $product_type->description ?>
+                </p>
+                <input type="button" value="<?php echo $product_type->name ?> Stuff" class="homebutton" id="go-to-post" onClick="document.location.href='<?php echo esc_url(get_term_link($product_type)) ?>'" />
+
+            </div>
+            <?php endforeach ?>
+        </div>
 
 
-		<!-- show the latest 3 posts -->
-		<h2 class="heading-journal">INHABITENT JOURNAL</h2>
-		<div class="latest-journal">
-		<?php $journal_posts = inhabitent_get_latest_posts(); ?>
-		<?php foreach ($journal_posts as $post) : setup_postdata($post); ?>
-		<?php get_template_part('template-parts/content-latest-post'); ?>
-		<?php endforeach;
-		wp_reset_postdata(); ?>
-		</div>
-		
-		<!-- show the latest 4 advantures in grid -->
-		<h2 class="heading-adventure">LATEST ADVENTURES</h2>
-		<div class="latest-adventure">
-		<?php $adventures= inhabitent_get_adventures();?>
-		<?php foreach($adventures as $post): setup_postdata($post);?>
-		<?php 
-			// the_post_thumbnail('medium_large');
-			// the_title();
-			get_template_part('template-parts/content-adventure');
-		?>
-		<?php endforeach; wp_reset_postdata(); ?>
-		</div>
-		<div class="entry-button">
-		<input type="button" value="More Adventures" class="homebutton" id="go-to-post" 
-onClick="document.location.href='<?php echo esc_url( get_post_type_archive_link( 'adventure' ))?>'" />
+        <!-- show the latest 3 posts -->
+        <h2 class="heading-journal">INHABITENT JOURNAL</h2>
+        <div class="latest-journal">
+            <?php $journal_posts = inhabitent_get_latest_posts(); ?>
+            <?php foreach ($journal_posts as $post): setup_postdata($post); ?>
+            <?php get_template_part('template-parts/content-latest-post'); ?>
+            <?php endforeach;
+        wp_reset_postdata(); ?>
+        </div>
 
-	</div><!-- .entry-button -->
-	
-	</main><!-- #main -->
-	</div><!-- #primary -->
-<?php get_footer(); ?>
+        <!-- show the latest 4 advantures in grid -->
+        <h2 class="heading-adventure">LATEST ADVENTURES</h2>
+        <div class="latest-adventure">
+            <?php $adventures = inhabitent_get_adventures(); ?>
+            <?php foreach ($adventures as $post): setup_postdata($post); ?>
+            <?php 
+            // the_post_thumbnail('medium_large');
+            // the_title();
+            get_template_part('template-parts/content-adventure');
+            ?>
+            <?php endforeach;
+        wp_reset_postdata(); ?>
+        </div>
+        <div class="button-more-adventure">
+            <input type="button" value="More Adventures" onClick="document.location.href='<?php echo esc_url(get_post_type_archive_link('adventure')) ?>'" />
+
+        </div><!-- .entry-button -->
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+<?php get_footer(); ?> 
--- themes/inhabitent/template-parts/content-adventure.php
+++ themes/inhabitent/template-parts/content-adventure.php
@@ -8,25 +8,12 @@
 ?>
 
 <article id="adventure-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<div class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+    <?php if (has_post_thumbnail()): ?>
+    <?php the_post_thumbnail('large'); ?>
+    <?php endif; ?>
+    <div class="adventure-title-button">
+        <?php the_title(sprintf('<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h3>'); ?>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php inhabitent_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> 
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-
-		<?php the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' ); ?>
-
-		
-</div><!-- .entry-header -->
-
-	<div class="entry-button">
-		<input type="button" value="READ MORE" class="homebutton" id="go-to-post" 
-onClick="document.location.href='<?php echo esc_url( get_permalink())?>'" />
-	</div><!-- .entry-button -->
-</article><!-- #post-## -->
-
+        <input type="button" value="READ MORE" class="homebutton" id="go-to-post" onClick="document.location.href='<?php echo esc_url(get_permalink()) ?>'" />
+    </div><!-- .entry-button -->
+</article><!-- #post-## --> 
