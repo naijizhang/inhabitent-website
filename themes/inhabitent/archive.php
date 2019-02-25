071790798606d2bb2eb467a681a6de8806a604bd
Naiji Zhang <naijizhang@gmail.com>
2019-02-25 15:43:52 -0800

    Add product type archive styling

--- themes/inhabitent/archive.php
+++ themes/inhabitent/archive.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying archive pages.
  *
@@ -7,37 +8,22 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="product-archive-heading single-type">
+    <h1 class="page-title">
+        <?php single_cat_title(); ?>
+    </h1>
+    <?php
+    the_archive_description('<div class="taxonomy-description">', '</div>');
+    ?>
+</div>
 
-		<?php if ( have_posts() ) : ?>
+<div class="product-container">
+    <?php if (have_posts()): ?>
+    <?php  /* Start the Loop */ ?>
+    <?php while (have_posts()): the_post(); ?>
+    <?php get_template_part('template-parts/content-product'); ?>
+    <?php endwhile; ?>
+    <?php endif; ?>
+</div>
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
-
-			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-
-<?php get_footer(); ?>
+<?php get_footer(); ?> 
