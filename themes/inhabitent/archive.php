9512b9bc8db9ff7058a01cb2cbcb411daacd7e30
Naiji Zhang <naijizhang@gmail.com>
2019-03-04 13:24:03 -0800

    Fix archive.php missing and add styling for archive page

--- themes/inhabitent/archive.php
+++ themes/inhabitent/archive.php
@@ -8,22 +8,23 @@
 
 get_header(); ?>
 
-<div class="product-archive-heading single-type">
-    <h1 class="page-title">
-        <?php single_cat_title(); ?>
-    </h1>
-    <?php
-    the_archive_description('<div class="taxonomy-description">', '</div>');
-    ?>
-</div>
+<div id="primary" class="journal-page-content archive-content">
+    <main id="main" class="journal-site-main archive-site-main" role="main">
+<h1>
+<?php the_archive_title();?>
+</h1>
+    <?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 
-<div class="product-container">
-    <?php if (have_posts()): ?>
-    <?php  /* Start the Loop */ ?>
-    <?php while (have_posts()): the_post(); ?>
-    <?php get_template_part('template-parts/content-product'); ?>
-    <?php endwhile; ?>
-    <?php endif; ?>
-</div>
+				<?php
+					get_template_part( 'template-parts/content' );
+				?>
+
+			<?php endwhile; ?>
+    </main><!-- #main -->
+
+    <?php get_sidebar(); ?> <!-- sidebar -->
+
+</div><!-- #primary -->
 
 <?php get_footer(); ?> 
