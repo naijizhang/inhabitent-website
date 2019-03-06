d005ebb44bfa12e71c054bce06c3141fffd4db59
Naiji Zhang <naijizhang@gmail.com>
2019-03-06 10:32:33 -0800

    Fix search result infinite loop

--- themes/inhabitent/template-parts/content-search.php
+++ themes/inhabitent/template-parts/content-search.php
@@ -9,25 +9,19 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())), '</a></h2>'); ?>
-    <?php if ('post' === get_post_type()): ?>
-        <div class="entry-meta">
-            <?php inhabitent_posted_on(); ?> /
-            <?php comments_number('0 Comments', '1 Comment', '% Comments'); ?> /
-            <?php inhabitent_posted_by(); ?>
-        </div><!-- .entry-meta -->
-        <?php endif; ?>
-	<div class="journal-content entry-summary">
+    <?php if('none'===get_post_type()):?>
+    <h1 class="page-title">
+        <?php printf(esc_html('Search Results for: %s'), '<span>' . get_search_query() . '</span>'); ?>
+    </h1>
+    <br>
+    <p>Cannot find...</p>
+    <?php else : ?>
+    <div class="journal-content entry-summary">
         <?php the_excerpt(); ?>
     </div><!-- .entry-summary -->
-    <?php if (have_posts()): ?>
     <div class="journal-button">
         <input type="button" value="READ ENTRY &rarr;" class="homebutton" id="go-to-post" onClick="document.location.href='<?php echo esc_url(get_permalink()) ?>'" />
     </div><!-- .entry-button -->
-<?php else:?>
-<h1 class="page-title">
-                <?php printf(esc_html('Search Results for: %s'), '<span>' . get_search_query() . '</span>'); ?>
-            </h1>
-            <br>
-            <p>Cannot find...</p>
+   
     <?php endif ?>
 </article><!-- #post-## --> 
