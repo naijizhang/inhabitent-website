e2065179c02f19063ab26dc6ed753c0c275b8147
Naiji Zhang <naijizhang@gmail.com>
2019-03-06 10:15:32 -0800

    Update search button js function and fix the keydown bug

--- themes/inhabitent/js/search.js
+++ themes/inhabitent/js/search.js
@@ -1,18 +1,25 @@
 jQuery(document).ready(function($) {
   $('header .search-field').attr('placeholder', 'TYPE AND HIT ENTER...');
   $('header .search-field').hide();
-
-  $('header .search-submit').on('click', function(event) {
-    event.preventDefault();
+//click--- show, click again ---hide
+  $('header .search-submit').toggle(function(){
     $('header .search-field').show('slow');
     $('header .search-field').focus();
+  }, function() {
+    $('header .search-field').hide('slow');
   });
+  
+
   $(document).on('keypress', function(event) {
-    if ($('header .search-field').val() !== '') {
+//if press enter, search
+    if ( event.which === 13 && $('header .search-field').val() !== '') {
       $('.search-form').submit();
     }
   });
+  //if the search field is empty click other place and hide the search field
   $('header .search-field').on('blur', function() {
-    $('header .search-field').hide(500);
+    if ($('header .search-field').val() === '') {
+      $('header .search-field').hide(500);
+    }
   });
 });
